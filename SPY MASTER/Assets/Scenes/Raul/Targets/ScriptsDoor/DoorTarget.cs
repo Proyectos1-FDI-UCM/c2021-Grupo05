7e97fae5743e79df09d6b3bd35066f81ea62450d
RaulSaavedraRiera <rsaave02@ucm.es>
2021-04-20 23:36:57 +0200

    Añadidos láser, electricidad y sistema de puertas

---
+++ SPY MASTER/Assets/Scenes/Raul/Targets/ScriptsDoor/DoorTarget.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+public class DoorTarget : MonoBehaviour
+{
+    [SerializeField]
+    GameObject doorBlock;
+
+    [SerializeField]
+    [Header("Tipo de puerta")]
+    bool redDoor;
+    [SerializeField]
+    bool blueDoor;
+    [SerializeField]
+    bool yellowDoor;
+
+    void CheckOpen(PlayerTarget player)
+    {
+        if (redDoor && player.redTarget)
+        {
+            doorBlock.SetActive(false);
+            player.redTarget = false;
+        }
+        else if (blueDoor && player.blueTarget)
+        {
+            doorBlock.SetActive(false);
+            player.blueTarget = false;
+        }
+        else if (yellowDoor && player.yellowTarget)
+        {
+            doorBlock.SetActive(false);
+            player.yellowTarget = false;
+        }
+    }
+
+    
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Contacto");
+        PlayerTarget player = collision.gameObject.GetComponentInChildren<PlayerTarget>();
+        if (player != null)
+            CheckOpen(player);
+    }
+}
---
+++ SPY MASTER/Assets/Scenes/Raul/Targets/ScriptsDoor/PlayerTarget.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class PlayerTarget : MonoBehaviour
+{
+    public bool redTarget, blueTarget, yellowTarget;
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        TargetIdenty target = collision.gameObject.GetComponent<TargetIdenty>();
+        
+        if (target != null)
+        {
+           if(target.redTarget)
+            {
+                if(!redTarget)
+                {
+                    redTarget = true;
+                    Destroy(collision.gameObject);
+                }
+            }
+           else if(target.blueTarget)
+            {
+                if (!blueTarget)
+                {
+                    blueTarget = true;
+                    Destroy(collision.gameObject);
+                }
+            }
+           else
+            {
+                if (!yellowTarget)
+                {
+                    yellowTarget = true;
+                    Destroy(collision.gameObject);
+                }
+            }
+        }
+    }
+}
