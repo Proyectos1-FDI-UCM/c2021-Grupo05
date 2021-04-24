195459417398b45fd2628db523906eb99b2813ea
RaulSaavedraRiera <rsaave02@ucm.es>
2021-04-24 11:23:49 +0200

    Reorganización del proyecto

---
+++ SPY MASTER/Assets/Scripts/PickUp/PickUpCol.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class PickUpCol : MonoBehaviour
+{
+    public GameObject interactionSymbol;
+
+    PickUpHistory information;
+    private void Start()
+    {
+        information = GetComponent<PickUpHistory>();
+    }
+  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("ColBeProduced");
+        if (collision.gameObject.tag == "Player")
+        {
+            interactionSymbol.SetActive(true);            
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {           
+            interactionSymbol.SetActive(false);
+            information.Close();
+        }
+    }
+
+    private void Update()
+    {
+        if(interactionSymbol.activeSelf && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("GetInformation");
+            information.GetInfo();
+        }
+    }
+}
