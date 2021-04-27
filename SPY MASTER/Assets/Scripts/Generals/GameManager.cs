363e5c88264efe8aae8bf0507243e9b3105a9b6b
Lauu029 <lauragbodego@gmail.com>
2021-04-27 22:39:28 +0200

    Creado el GameManager y comenzado el UiManager

---
+++ SPY MASTER/Assets/Scripts/Generals/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //Variable que almacena la instancia del GameManager
+    private static GameManager instance;
+    /*Al activar el objeto asociado, evita que haya dos controladores del GameManager*/
+    private void Awake()
+    {
+        //Si no tiene instancia ya creada, almacena la actual
+        if (instance == null)
+        {
+            instance = this;
+
+            //No destruye el Game Manager aunque se cambie de escena  
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            //Si ya hay instancia creada lo destruye porque no se necesita otro
+            Destroy(this);
+        }
+    }
+    //Devuelve la instancia del GameManager
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
