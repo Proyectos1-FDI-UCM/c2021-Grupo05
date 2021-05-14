8e305b544327ed7216e2643ce752aeba7e789f0f
RaulSaavedraRiera <rsaave02@ucm.es>
2021-05-14 12:03:23 +0200

    Añadido script para dibujar target
    Ahora se pueden ver los target en la vista scene para una colocación más sencilla a la hora de desarrollar niveles

---
+++ SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/TargetOfLaser.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Raúl Saavedra de la Riera
+public class TargetOfLaser : MonoBehaviour
+{
+  
+    private void OnDrawGizmos()
+    {
+     
+        Gizmos.color = Color.red;
+        Gizmos.DrawCube(this.gameObject.transform.position, new Vector2(0.25f,0.25f));
+    }
+}
