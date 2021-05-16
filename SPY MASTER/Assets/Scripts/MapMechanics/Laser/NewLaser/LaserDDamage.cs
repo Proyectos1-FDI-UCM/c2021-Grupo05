14bd268a683d9d81e3ddfaee3540d7a8bf508134
RaulSaavedraRiera <rsaave02@ucm.es>
2021-05-16 20:52:29 +0200

    Láser funcional capaz de hacer daño al jugador implementado con su correspondiente prefab

--- SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/LaserDDamage.cs
+++ SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/LaserDDamage.cs
@@ -9,29 +9,49 @@ public class LaserDDamage : MonoBehaviour
     [SerializeField]
     LineRenderer lr;
 
+    [SerializeField]
+    float velocityDamage;
+    
+    [SerializeField]
     LayerMask layerObj;
     // Start is called before the first frame update
 
     // Update is called once per frame
+    float lastDamage = 0f;
 
     void Start()
     {
-        layerObj = LayerMask.NameToLayer("Player");
+       
         lr.startColor = lr.endColor = Color.red;
         
     }
     void FixedUpdate()
     {
-        RaycastHit hit;
-        Vector3 tmp = target.transform.position - transform.position;
-
-        lr.SetPosition(0, transform.position);
-        lr.SetPosition(1, target.transform.position);
-        if (Physics.Raycast(transform.position, transform.TransformDirection(tmp), out hit, Mathf.Infinity, layerObj))
-        {           
-            Debug.Log("Did Hit");
-        }
       
+            Vector3 dir = (target.transform.position - transform.position).normalized;
+
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, target.transform.position);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, dir, layerObj);
+
+        if (lastDamage + velocityDamage <= Time.timeSinceLevelLoad)
+        {
+            if (hit != false)
+            {
+                SimpleMov player = hit.collider.gameObject.GetComponent<SimpleMov>();
+                if (player != null)
+                {
+                    player.TakeDamage();
+                    lastDamage = Time.timeSinceLevelLoad;
+                }
+            }
+        }
+
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+      
+       
+       
+       // Debug.DrawLine(transform.position, transform.position + dir * 10, Color.red, Mathf.Infinity);
     }
     /*
      *  SimpleMov player = collision.gameObject.GetComponent<SimpleMov>();
