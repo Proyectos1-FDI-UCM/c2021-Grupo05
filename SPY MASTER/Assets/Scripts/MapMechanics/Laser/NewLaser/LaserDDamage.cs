0f5042cd5d9a216af61304429a852203080e2339
RaulSaavedraRiera <rsaave02@ucm.es>
2021-05-16 20:04:06 +0200

    Añadido script daño láser

--- SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/LaserDDamage.cs
+++ SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/LaserDDamage.cs
@@ -6,6 +6,8 @@ public class LaserDDamage : MonoBehaviour
 {
     [SerializeField]
     GameObject target;
+    [SerializeField]
+    LineRenderer lr;
 
     LayerMask layerObj;
     // Start is called before the first frame update
@@ -15,17 +17,21 @@ public class LaserDDamage : MonoBehaviour
     void Start()
     {
         layerObj = LayerMask.NameToLayer("Player");
+        lr.startColor = lr.endColor = Color.red;
+        
     }
     void FixedUpdate()
     {
         RaycastHit hit;
         Vector3 tmp = target.transform.position - transform.position;
 
+        lr.SetPosition(0, transform.position);
+        lr.SetPosition(1, target.transform.position);
         if (Physics.Raycast(transform.position, transform.TransformDirection(tmp), out hit, Mathf.Infinity, layerObj))
-        {
-            Debug.DrawRay(transform.position, transform.TransformDirection(tmp) * hit.distance, Color.yellow);
+        {           
             Debug.Log("Did Hit");
         }
+      
     }
     /*
      *  SimpleMov player = collision.gameObject.GetComponent<SimpleMov>();
--- SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/TargetOfLaser.cs
+++ SPY MASTER/Assets/Scripts/MapMechanics/Laser/NewLaser/TargetOfLaser.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 //Raúl Saavedra de la Riera
 public class TargetOfLaser : MonoBehaviour
 {
-  
+    [Header("Indicador rojo")]
+    [SerializeField]
+    bool red;
+    [Header("Indicador azul")]
+    [SerializeField]
+    bool blue;
+
     private void OnDrawGizmos()
     {
-     
+        if(blue)
+        Gizmos.color = Color.blue;
+        if (red) 
         Gizmos.color = Color.red;
         Gizmos.DrawCube(this.gameObject.transform.position, new Vector2(0.25f,0.25f));
     }
