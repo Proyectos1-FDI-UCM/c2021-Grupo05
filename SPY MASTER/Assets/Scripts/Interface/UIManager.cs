094b6d26a9718e1e205e7ea79ecf63493a6623b2
RaulSaavedraRiera <rsaave02@ucm.es>
2021-05-19 20:02:16 +0200

    Arreglados problemas varios de los menús
    Ahora no se continúan en las escenas
    Los niveles cargan correctamente

--- SPY MASTER/Assets/Scripts/Interface/UIManager.cs
+++ SPY MASTER/Assets/Scripts/Interface/UIManager.cs
@@ -2,22 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    //Raúl Saavedra
+    //Laura Gómez
+
+    [SerializeField]
+    Button pause;
     // Start is called before the first frame update
     void Start()
     {
         GameManager.GetInstance().SetUiManager(this);
     }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            //falta añadir la otra escena
+            if(SceneManager.GetActiveScene().name == "MergeScene")
+            
+                pause.onClick.Invoke();
+          
+        }
+    }
 
     public void CambiaEscena(string scene)
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(scene);
     }
     public void PausaNivel(int valor)
     {
         Time.timeScale = valor;
     }
- 
+    public void ChangeTime()
+    {
+        int a;
+        if (Time.timeScale == 0) a = 1;
+        else a = 0;
+        Time.timeScale = a;
+    }
+
+
 }
