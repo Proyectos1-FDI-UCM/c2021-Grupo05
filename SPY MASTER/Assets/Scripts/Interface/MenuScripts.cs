253eeaa2b02635fc75009f4261769fd815364245
RaulSaavedraRiera <rsaave02@ucm.es>
2021-05-20 19:01:05 +0200

    Arreglos en bugs de los menús 2: interacción entre todas las escenas

---
+++ SPY MASTER/Assets/Scripts/Interface/MenuScripts.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScripts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
+
