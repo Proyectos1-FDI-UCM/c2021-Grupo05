70a2e3b64ba27d47dc8c8376f5302954ad54d3fa
Jacobo <jacoboal@ucm.es>
2021-05-27 11:37:53 +0200

    Arregado el CanvasMelee

--- SPY MASTER/Assets/Scripts/Player/Detector.cs
+++ SPY MASTER/Assets/Scripts/Player/Detector.cs
@@ -18,11 +18,13 @@ public class Detector : MonoBehaviour
     //Lista de posibles enemigos en rango
     private List<Transform> enemInRange = new List<Transform>(0);
     private CanvasGroup canvasAlpha;
+    private RectTransform canvasRect;
 
     private void Start() 
     {
         canvasAlpha = meleeCanvas.GetComponent<CanvasGroup>();
-        meleeCanvas.parent = null;
+        canvasRect = meleeCanvas.GetComponent<RectTransform>();
+        meleeCanvas.SetParent(null);
     }
 
     private void OnTriggerEnter2D(Collider2D col) //usar if(col is Soldier) en un Futuro?
@@ -45,29 +47,22 @@ public class Detector : MonoBehaviour
 
     private void Update() 
     {
-        if(canvasAlpha != null)
+        canvasAlpha.alpha = enemInRange.Count > 0 ? 1 : 0;
+        if (enemInRange.Count > 0)
         {
-
-            canvasAlpha.alpha = enemInRange.Count > 0 ? 1 : 0;
-            if (enemInRange.Count > 0)
-            {
-                meleeCanvas.SetParent(GetNearestEnemy(), false);
-            }
-            //Al morir los enemigos desactivan collider y esperan un lapso de animacion de muerte.
-            //Detach el canvas en ese momento para no ser eliminado con los enemigos (va a ver que cambiar esto si o si /con eventos?)
-            else
-            {
-                meleeCanvas.SetParent(null, false);
-            }
-
-
+            meleeCanvas.SetParent(GetNearestEnemy(), true);
+            canvasRect.anchoredPosition = new Vector3(0,0,0);
         }
-        
+        //Al morir los enemigos desactivan collider y esperan un lapso de animacion de muerte.
+        //Detach el canvas en ese momento para no ser eliminado con los enemigos (va a ver que cambiar esto si o si /con eventos?)
+        else
+        {
+            meleeCanvas.SetParent(null, true);
+        }
     }
 
     public Transform GetNearestEnemy()
     {
-
         float minDistance = Mathf.Infinity;
         float distanceBetween;
         Transform enemToReturn = null;
@@ -80,8 +75,6 @@ public class Detector : MonoBehaviour
                 enemToReturn = enem;
             }
         }
-
-        Debug.Log("enemToReturn = " + enemToReturn);
         return enemToReturn;
     }
 }
